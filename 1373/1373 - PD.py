0c109e98bb3af29e5983cca2236b15374b2d5b01
Velmer <velmer.odon@ccc.ufcg.edu.br>
2018-02-20 00:26:23 -0300

    refact

--- 1373/1373 - PD.py
+++ 1373/1373 - PD.py
@@ -4,26 +4,22 @@ while k != 0:
     x = raw_input()
     y = raw_input()
 
-    matriz = [[0]*(len(y)+1) for z in xrange(len(x)+1)]
+    n = len(x)
+    m = len(y)
 
-    for i in xrange(1, len(x)+1):
-        for j in xrange(1, len(y)+1):
+    matriz = [[0]*(m+1) for z in xrange(n+1)]
+
+    for i in xrange(1, n+1):
+        for j in xrange(1, m+1):
             matriz[i][j] = max(matriz[i-1][j], matriz[i][j-1])
             tam_seg = 0
 
             while (tam_seg <= i - 1 and tam_seg <= j - 1 and x[i-tam_seg-1] == y[j-tam_seg-1]):
                 tam_seg += 1
 
-            if (tam_seg >= k):
-                matriz[i][j] = max(matriz[i][j], matriz[i-tam_seg][j-tam_seg] + tam_seg)
+                if (tam_seg >= k):
+                    matriz[i][j] = max(matriz[i][j], matriz[i-tam_seg][j-tam_seg] + tam_seg)
 
-    print
-
-    for linha in matriz:
-        print linha
-    
-    print
-
-    print matriz[len(x)][len(y)]
+    print matriz[n][m]
 
     k = int(raw_input())
